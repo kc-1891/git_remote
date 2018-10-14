4a94ad7db9a75d29336eca49d8ed2703b1ec49e5
Kumaran <kumaran.cb@gmail.com>
2018-10-14 19:22:32 +0100

    my second edits from dev user1

--- demo.py
+++ demo.py
@@ -2,4 +2,4 @@ print("welcome to git world")
 print("fir edit")
 print("This is the edit from github")
 print("This is the edit by another user on same file")
-my first edits from branch dev
+my first edits from branch dev(edited again from dev user1)
